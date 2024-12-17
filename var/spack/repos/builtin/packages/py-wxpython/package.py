5bac3b27b4aa700495d83ea4c5dd9af2810c8e22
Brendan Moloney <moloney.brendan@gmail.com>
2024-12-16 18:07:41 -0800

    ENH: Add wxwidgets 3.2.6 and py-wxpython 4.2.2
    Improves compat with newer Python (>3.9) and numpy

--- var/spack/repos/builtin/packages/py-wxpython/package.py
+++ var/spack/repos/builtin/packages/py-wxpython/package.py
@@ -12,6 +12,7 @@ class PyWxpython(PythonPackage):
     homepage = "https://www.wxpython.org/"
     pypi = "wxPython/wxPython-4.0.6.tar.gz"
 
+    version("4.2.2", sha256="5dbcb0650f67fdc2c5965795a255ffaa3d7b09fb149aa8da2d0d9aa44e38e2ba")
     version("4.1.1", sha256="00e5e3180ac7f2852f342ad341d57c44e7e4326de0b550b9a5c4a8361b6c3528")
     version("4.0.6", sha256="35cc8ae9dd5246e2c9861bb796026bbcb9fb083e4d49650f776622171ecdab37")
 
@@ -19,6 +20,7 @@ class PyWxpython(PythonPackage):
     depends_on("cxx", type="build")  # generated
 
     depends_on("wxwidgets")
+    depends_on("wxwidgets@3.2.6", when="@4.2.2")
 
     # Needed for the build.py script
     depends_on("py-setuptools", type="build")
